99dfece03ce783a445def4792f9f3d36c15355d1
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-14 02:35:10 +0500

    Add project files.

---
+++ CashOverflow.API/Program.cs
@@ -0,0 +1,23 @@
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace CashOverflow.API
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
