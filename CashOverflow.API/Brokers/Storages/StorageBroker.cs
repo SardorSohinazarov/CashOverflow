8d765edb5f5b41a9ee07a34206568c61390154b1
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-14 05:26:21 +0500

    CODE RUB : Code Cleaned

--- CashOverflow.API/Brokers/Storages/StorageBroker.cs
+++ CashOverflow.API/Brokers/Storages/StorageBroker.cs
@@ -7,10 +7,6 @@ using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
-// --------------------------------------------------------
-// Copyright (c) Coalition of Good-Hearted Engineers
-// Developed by me :)
-// --------------------------------------------------------
 namespace CashOverflow.API.Brokers.Storages
 {
     public partial class StorageBroker : EFxceptionsContext
