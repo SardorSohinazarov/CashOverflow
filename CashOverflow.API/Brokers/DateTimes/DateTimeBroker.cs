78a68fd71125a730e4160b43784adce3089d839c
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-16 04:51:25 +0500

    BROKER: Datetime Broker Add

---
+++ CashOverflow.API/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using System;
+
+namespace CashOverflow.API.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffSet() =>
+            DateTimeOffset.UtcNow;
+    }
+}
