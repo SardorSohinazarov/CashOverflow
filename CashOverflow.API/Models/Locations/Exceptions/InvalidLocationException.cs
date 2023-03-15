5e15e42b025ae86edc1aa9e266d48df8e2259111
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-16 02:09:13 +0500

    ShouldThrowValidationExceptionOnAddIfLocationIsInvalidAndLogItAsync -> FAIL

---
+++ CashOverflow.API/Models/Locations/Exceptions/InvalidLocationException.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using Xeptions;
+
+namespace CashOverflow.API.Models.Locations.Exceptions
+{
+    public class InvalidLocationException: Xeption
+    {
+        public InvalidLocationException()
+            : base(message:"Location is invalid.")
+        {}
+    }
+}
