86f54fb98e0b306498e6d321287ca6738ee20aaa
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-16 01:08:26 +0500

    CODE RUB: Refactored Method

---
+++ CashOverflow.API/Services/Foundations/Locations/LocationService.Validations.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using CashOverflow.API.Models.Locations.Exceptions;
+using CashOverflow.API.Models.Locations;
+
+namespace CashOverflow.API.Services.Foundations.Locations
+{
+    public partial class LocationService
+    {
+        private static void ValidateLocationNotNull(Location location)
+        {
+            if (location is null)
+            {
+                throw new NullLocationException();
+            }
+        }
+    }
+}
