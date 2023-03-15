9392bb7271e1b429dad1ff48bfc99abfc2637137
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-15 19:48:33 +0500

    CONFIGS: Logging Broker Added

---
+++ CashOverflow.API/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using System;
+
+namespace CashOverflow.API.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
