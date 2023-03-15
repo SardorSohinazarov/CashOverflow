9e4fb792f7607fdfda8311d94319bdc9c9a9c0f6
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-15 10:34:25 +0500

    FOUNDATION: Location Add

---
+++ CashOverflow.API/Services/Foundations/Locations/LocationService.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using System.Threading.Tasks;
+using CashOverflow.API.Brokers.Storages;
+using CashOverflow.API.Models.Locations;
+
+namespace CashOverflow.API.Services.Foundations.Locations
+{
+    public class LocationService : ILocationService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public LocationService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public async ValueTask<Location> AddLocationAsync(Location location) =>
+            await storageBroker.InsertLocationAsync(location);
+    }
+}
---
+++ CashOverflow.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using CashOverflow.API.Brokers.Storages;
+using CashOverflow.API.Models.Locations;
+using CashOverflow.API.Services.Foundations.Locations;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace CashOverflow.Infrastructure.Build.Services.Foundations.Locations
+{
+    public partial class LocationServiceTests
+    {
+        private readonly Mock<IStorageBroker> mockStorageBroker;
+        private readonly ILocationService locationService;
+
+        public LocationServiceTests()
+        {
+            this.mockStorageBroker = new Mock<IStorageBroker>();
+
+            this.locationService = new LocationService(this.mockStorageBroker.Object);
+        }
+
+
+        private static Location CreateRandomLocation() =>
+            CreateLocationFiller(dates: GetRandomDatetimeOffset()).Create();
+
+        private static Filler<Location> CreateLocationFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Location>();
+
+            filler.Setup().OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+
+        private static DateTimeOffset GetRandomDatetimeOffset() =>
+            new DateTimeRange(earliestDate:DateTime.UnixEpoch).GetValue();
+    }
+}
